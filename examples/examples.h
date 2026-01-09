37f2c2fae427f5d02b4bf4604ed0bdd416986929
Jean-Luc Watson <jwatson@nvidia.com>
2026-01-09 13:38:01 -0800

    Updated copyright headers -- happy new year!

--- examples/examples.h
+++ examples/examples.h
@@ -8,7 +8,7 @@
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Licensed under the MIT license.
  *
- * SPDX-FileCopyrightText: Copyright (c) 2023-2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+ * SPDX-FileCopyrightText: Copyright (c) 2023-2026 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
  * SPDX-License-Identifier: Apache-2.0
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
