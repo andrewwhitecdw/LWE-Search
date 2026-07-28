26be1dace33a7d8a1a1a03007465a4f97725080c
Andrew White <andrewh@cdw.com>
2026-07-27 20:54:54 -0500

    fix: cudaCheckError exits 0 on failure, hiding errors

--- src/test_gemm.cu
+++ src/test_gemm.cu
@@ -27,7 +27,7 @@
     if (e != cudaSuccess) {                                                    \
       printf("Cuda failure %s:%d: '%s'\n", __FILE__, __LINE__,                 \
              cudaGetErrorString(e));                                           \
-      exit(0);                                                                 \
+      exit(1);                                                                 \
     }                                                                          \
   }
 
